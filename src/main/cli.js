28a07197c6eb20e0c45b92aaff67e2482d7e9285
Giancarlo Anemone <ganemone@uber.com>
2019-02-19 18:42:12 -0500

    Split up a cli export and a main export

---
+++ src/main/cli.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Uber Node.js
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/* eslint-disable no-process-exit */
+// @flow
+
+import 'source-map-support/register';
+
+import 'babel-polyfill';
+import path from 'path';
+import fs from 'fs';
+import yargs from 'yargs';
+
+import mkdirp from 'mkdirp';
+import convert from './index';
+
+const argv = yargs
+  .usage('Usage: $0 [options] <thrift files..>')
+  .option('suffix', {
+    describe: 'appended to generated type names',
+    default: 'Type',
+  })
+  .option('path', {
+    describe: 'Path used for generated code',
+  })
+  .option('withsource', {
+    describe: 'prepend the source path of the thrift file',
+    default: false,
+  })
+  .help('h')
+  .alias('h', 'help').argv;
+
+const thriftPaths = argv._;
+
+if (!thriftPaths.length) {
+  yargs.showHelp();
+  process.exit(1);
+}
+
+const options = {
+  suffix: argv.suffix,
+  withSource: argv.withsource,
+  commonPath: argv.path,
+};
+
+const allOutput = convert(thriftPaths, options);
+
+for (const jsFilename in allOutput) {
+  mkdirp(path.dirname(jsFilename), () =>
+    fs.writeFile(jsFilename, allOutput[jsFilename], () =>
+      console.log(`Wrote ${jsFilename}`)
+    )
+  );
+}
--- src/main/index.js
+++ src/main/index.js
@@ -1,92 +1,47 @@
-#!/usr/bin/env node
-/*
- * MIT License
- *
- * Copyright (c) 2017 Uber Node.js
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-/* eslint-disable no-process-exit */
 // @flow
 
-import 'source-map-support/register';
-
-import 'babel-polyfill';
-import path from 'path';
-import fs from 'fs';
-import yargs from 'yargs';
-
-import commonPathPrefix from 'common-path-prefix';
-import mkdirp from 'mkdirp';
 import {ThriftFileConverter} from './convert';
+import commonPathPrefix from 'common-path-prefix';
+import path from 'path';
 
-const argv = yargs
-  .usage('Usage: $0 [options] <thrift files..>')
-  .option('suffix', {
-    describe: 'appended to generated type names',
-    default: 'Type',
-  })
-  .option('path', {
-    describe: 'Path used for generated code',
-  })
-  .option('withsource', {
-    describe: 'prepend the source path of the thrift file',
-    default: false,
-  })
-  .help('h')
-  .alias('h', 'help').argv;
+type OptionsType = {
+  suffix?: string,
+  withSource?: boolean,
+  commonPath?: string,
+  outputDir?: string,
+};
 
-const thriftPaths = argv._;
-
-if (!thriftPaths.length) {
-  yargs.showHelp();
-  process.exit(1);
-}
-
-const allOutput = {};
-
-for (const thriftPath of thriftPaths) {
-  const converter = new ThriftFileConverter(
-    thriftPath,
-    name => name + argv.suffix,
-    argv.withsource
-  );
-  converter
-    .getImportAbsPaths()
-    .filter(p => thriftPaths.indexOf(p) === -1)
-    .forEach(p => thriftPaths.push(p));
-  allOutput[converter.thriftPath] = converter.generateFlowFile();
-}
-
-const root = argv.path || commonPathPrefix(Object.keys(allOutput));
-
-for (const thriftPath in allOutput) {
-  const relativeThriftPath = path.dirname(path.relative(root, thriftPath));
-  const jsFilename = path.resolve(
-    'flow-output',
-    relativeThriftPath,
-    `${path.basename(thriftPath, '.thrift')}.js`
-  );
-  mkdirp(path.dirname(jsFilename), () =>
-    fs.writeFile(jsFilename, allOutput[thriftPath], () =>
-      console.log(`Wrote ${jsFilename}`)
-    )
-  );
+export default function convert(
+  thriftPaths: Array<string>,
+  options: OptionsType
+) {
+  const {
+    suffix = 'Type',
+    withSource = false,
+    outputDir = 'flow-output',
+    commonPath,
+  } = options;
+  const allOutput = {};
+  for (const thriftPath of thriftPaths) {
+    const converter = new ThriftFileConverter(
+      thriftPath,
+      name => name + suffix,
+      withSource
+    );
+    converter
+      .getImportAbsPaths()
+      .filter(p => thriftPaths.indexOf(p) === -1)
+      .forEach(p => thriftPaths.push(p));
+    const root = commonPath || commonPathPrefix(Object.keys(allOutput));
+    const relativeThriftPath = path.dirname(
+      path.relative(root, converter.thriftPath)
+    );
+    const jsFilename = path.resolve(
+      outputDir,
+      relativeThriftPath,
+      `${path.basename(thriftPath, '.thrift')}.js`
+    );
+    allOutput[jsFilename] = converter.generateFlowFile();
+  }
+  return allOutput;
 }
