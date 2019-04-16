8a1deab4a331f8634c027beadfda4deda69693ef
Giancarlo Anemone <ganemone@uber.com>
2019-04-16 17:03:48 -0400

    Add back enum logic

--- src/__tests__/consts/const-lists.test.js
+++ src/__tests__/consts/const-lists.test.js
@@ -52,14 +52,14 @@ export const Direction: $ReadOnly<{|
   RIGHT: \\"RIGHT\\"
 });
 
-export const DIRECTIONS: number[] = [
+export const DIRECTIONS: $Values<typeof Direction>[] = [
   Direction.LEFT,
   Direction.RIGHT,
   Direction.LEFT,
   Direction.RIGHT
 ];
 
-export const DIRECTIONS_LIST: number[] = [
+export const DIRECTIONS_LIST: $Values<typeof Direction>[] = [
   Direction.LEFT,
   Direction.RIGHT,
   Direction.LEFT,
